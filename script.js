c2e1c9fe09c96695e5b178af8cf86db1c057b27a
Shubh Desai <shubhdesai2023@gmail.com>
2024-07-22 18:37:39 +0530

    Create placeholders for all DOM elements in script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById("display");
+
+const clear = document.getElementById("clear");
+const backspace = document.getElementById("backspace");
+
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+const negate = document.getElementById("negate");
+const equal = document.getElementById("equal");
+
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const zero = document.getElementById("zero");
+
+const point = document.getElementById("point");
+
+let operand1 = "";
+let operand2 = "";
+let operator = "";
