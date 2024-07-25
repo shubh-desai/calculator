6e94ca01d8d8a09812f6062b81036bcd475d0c04
Shubh Desai <shubhdesai2023@gmail.com>
2024-07-25 10:29:06 +0530

    Add more styling and attach dummy event listeners to the buttons

--- script.js
+++ script.js
@@ -23,6 +23,95 @@ const zero = document.getElementById("zero");
 
 const point = document.getElementById("point");
 
-let operand1 = "";
+let operand1 = "0";
 let operand2 = "";
 let operator = "";
+let operatorAdded = false;
+
+one.addEventListener("click", appendOperand);
+two.addEventListener("click", appendOperand);
+three.addEventListener("click", appendOperand);
+four.addEventListener("click", appendOperand);
+five.addEventListener("click", appendOperand);
+six.addEventListener("click", appendOperand);
+seven.addEventListener("click", appendOperand);
+eight.addEventListener("click", appendOperand);
+nine.addEventListener("click", appendOperand);
+zero.addEventListener("click", appendOperand);
+
+add.addEventListener("click", appendOperator);
+subtract.addEventListener("click", appendOperator);
+multiply.addEventListener("click", appendOperator);
+divide.addEventListener("click", appendOperator);
+negate.addEventListener("click", appendOperator);
+equal.addEventListener("click", appendOperator);
+
+function appendOperand(event) {
+
+    let number;
+
+    switch (event.target.id) {
+        case "one":
+            number = 1;
+            break;
+        case "two":
+            number = 2;
+            break;
+        case "three":
+            number = 3;
+            break;
+        case "four":
+            number = 4;
+            break;
+        case "five":
+            number = 5;
+            break;
+        case "six":
+            number = 6;
+            break;
+        case "seven":
+            number = 7;
+            break;
+        case "eight":
+            number = 8;
+            break;
+        case "nine":
+            number = 9;
+            break;
+        case "zero":
+            number = 0;
+            break;
+    }
+
+    // console.log(event.target.id);
+    // console.log(number);
+}
+
+function appendOperator(event) {
+
+    let curr_operator;
+
+    switch (event.target.id) {
+        case "add":
+            curr_operator = "+";
+            break;
+        case "subtract":
+            curr_operator = "-";
+            break;
+        case "multiply":
+            curr_operator = "*";
+            break;
+        case "divide":
+            curr_operator = "/";
+            break;
+        case "negate":
+            curr_operator = "?";
+            break;
+        case "equal":
+            curr_operator = "=";
+            break;
+    }
+
+    // console.log(event.target.id);
+    // console.log(curr_operator);
+}
